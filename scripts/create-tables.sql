1c495af5b1d2219e7bda2ff91467992ac9a63dcb
Philipp Kolmann <philipp@kolmann.at>
2021-05-18 14:29:06 +0200

    Add house_attribute from export to database

--- scripts/create-tables.sql
+++ scripts/create-tables.sql
@@ -20,6 +20,7 @@ CREATE TABLE bev_addresses
   house_name character varying,
   address_type character varying,
   municipality_has_ambiguous_addresses boolean NOT NULL DEFAULT FALSE,
+  house_attribute character varying,
   point geography(Point,4326) NOT NULL
 );
 
@@ -31,3 +32,15 @@ CREATE INDEX bev_addresses_municipality ON bev_addresses(municipality);
 CREATE INDEX bev_addresses_locality ON bev_addresses(locality);
 CREATE INDEX bev_addresses_street ON bev_addresses(street);
 CREATE INDEX bev_addresses_house_number ON bev_addresses(house_number);
+
+comment on column bev_addresses.house_attribute is "Überwiegende Eigenschaft dieses Objektes:
+01: Gebäude mit einer Wohnung
+02: Gebäude mit zwei oder mehr Wohnungen
+03: Wohngebäude für Gemeinschaften
+04: Hotels und ähnliche Gebäude
+05: Bürogebäude
+06: Groß- und Einzelhandelsgebäude
+07: Gebäude des Verkehrs- und Nachrichtenwesens
+08: Industrie- und Lagergebäude
+09: Gebäude für Kultur- und Freizeitzwecke sowie das Bildungs- und Gesundheitswesen";
+
